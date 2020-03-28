042f3f2687eb52d2b2d88b3cf44a148751a42b0e
st8117 <st8117@gmail.com>
2020-03-28 17:25:26 +0800

    assdf

--- build/deploy-prod.sh
+++ build/deploy-prod.sh
@@ -4,4 +4,4 @@ sfdx force:source:convert -d deploy_prod -r force-app
 #Deploy to prod & run all tests
 echo "Deploying to production & running all tests..."
 #sfdx force:mdapi:deploy -u DevHub -d deploy_prod/ -w -1 -l RunAllTestsInOrg
-sfdx force:mdapi:deploy -u DevHub -d deploy_prod/ -w -1 -l
+sfdx force:mdapi:deploy -u DevHub -d deploy_prod/ 
